c69149ba8f2b65dcd4dd6895cf3fb5e552b77d91
rguedes28 <87499994+rguedes28@users.noreply.github.com>
2021-10-21 17:06:46 -0300

    schema e niveis de tests concluidos

---
+++ src/test/java/br/com/restassuredapitesting/runners/AccptanceTests.java
@@ -0,0 +1,15 @@
+package br.com.restassuredapitesting.runners;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(br.com.restassuredapitesting.suites.AccptanceTests.class)
+@Suite.SuiteClasses({
+        AccptanceTests.class,
+
+
+})
+public class AccptanceTests {
+}
--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
@@ -2,11 +2,16 @@ package br.com.restassuredapitesting.tests.booking.tests;
 
 import br.com.restassuredapitesting.base.BaseTest;
 import br.com.restassuredapitesting.suites.AllTests;
+import br.com.restassuredapitesting.suites.ContractTests;
 import br.com.restassuredapitesting.tests.booking.requests.GetBookingRequest;
 
+import br.com.restassuredapitesting.utils.Utils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import java.io.File;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class GetBookingTest extends BaseTest {
@@ -23,6 +28,16 @@ public class GetBookingTest extends BaseTest {
                 .body("size()", greaterThan(0));
 
 
+    }
+
+    @Test
+    @Category({AllTests.class,ContractTests.class})
+    public void validaSchemaDaListagemDeReservas(){
+
+        getBookingRequest.bookingReturnIds()
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchema(new File(Utils.getSchemaBasePath("booking","bookings"))));
     }
 
 }
